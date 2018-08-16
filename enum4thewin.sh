a2a9bfd01cfb51c324159e9ff764850cab58521c
Will Gould <will.gould@cyberis.co.uk>
2018-08-16 10:01:49 +0100

    Change file name

--- enum4thewin.sh
+++ enum4thewin.sh
@@ -2,5 +2,5 @@
 
 while read line
 do
-  grep '445/open' | awk '{ print $2 }' | sort -u | tee -a Hosts_smb.txt | for i in $(cat Hosts_smb);do enum4linux -a $i 
+  grep '445/open' | awk '{ print $2 }' | sort -u | tee -a Hosts_smb.txt | for i in $(cat Hosts_smb.txt);do enum4linux -a $i 
 done < "${1:-/dev/stdin}"
